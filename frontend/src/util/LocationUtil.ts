2308d7be78aa9fd1664f3c14cd73fe1b3a71fe44
sepfCampus <sebastian.pfeiffer@stud.fh-campuswien.ac.at>
2026-01-16 02:52:32 +0100

    Exam/Done Added:  * Redrects to the exam pages

--- frontend/src/util/LocationUtil.ts
+++ frontend/src/util/LocationUtil.ts
@@ -3,6 +3,8 @@ class LocationUtil
     private URL_REDIRECT_BASE = "/src/pages";
     private URL_REDIRECT_LOGIN_PAGE = this.URL_REDIRECT_BASE + "/login/login.html";
     private URL_REDIRECT_USER_PAGE = this.URL_REDIRECT_BASE + "/user/user.html";
+    private URL_REDIRECT_EXAM_EXECUTOR_PAGE = this.URL_REDIRECT_BASE + "/exam/executor/exam.html";
+    private URL_REDIRECT_EXAM_EDITOR_PAGE = this.URL_REDIRECT_BASE + "/exam/editor/examEditor.html";
     private URL_REDIRECT_MAIN_PAGE = this.URL_REDIRECT_BASE + "";
 
     redirect(location:string, params?:any)
@@ -33,6 +35,16 @@ class LocationUtil
     redirectToMainPage()
     {
         this.redirect(this.URL_REDIRECT_MAIN_PAGE);
+    }
+
+    redirectToExamExecutorPage(id:number)
+    {
+        this.redirect(this.URL_REDIRECT_EXAM_EXECUTOR_PAGE, { examId: id });
+    }
+
+    redirectToExamEditorPage(id:number|null = null)
+    {
+        this.redirect(this.URL_REDIRECT_EXAM_EDITOR_PAGE, (id ? { examId: id } : undefined));
     }
 }
 
